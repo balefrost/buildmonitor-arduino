bd65398314af6704426b8399221a720d730b8c4f
Daniel Yankowsky <bytecrafter@gmail.com>
2013-04-01 11:49:00 -0400

    pixels show their age

--- CellularAutomata/CellularAutomata.ino
+++ CellularAutomata/CellularAutomata.ino
@@ -1,42 +1,45 @@
 #include <SPI.h>
+#include "RgbColor.h"
 
 byte rule = 30;
 
-uint32_t oldBits;
-uint32_t newBits;
+struct State {
+	uint32_t bits;
+	unsigned long birthDates[32];
+};
+
+struct State oldState;
+struct State newState;
 
 const unsigned long frameDuration = 1000;
 
 unsigned long thisFlip;
 
-inline bool setBit(uint32_t &var, size_t bit, bool state) {
-	bool result = (var >> bit) & 0x01;
-	if (state) {
-		var |= (0x01 << bit);
-	} else {
-		var &= (0x01 << bit);
-	}
-	return result;
-}
-
-inline bool getBit(uint32_t &var, size_t bit) {
+inline bool getBit(uint32_t var, size_t bit) {
 	return (var >> bit) & 0x01;
 }
 
-uint32_t calculateNewBits(uint32_t bits) {
-	uint32_t result = 0;
+rgb8 newColor(0xff, 0xff, 0x00);
+rgb8 oldColor(0x80, 0x00, 0x00);
+
+struct State calculateNewState(struct State oldState, unsigned long now) {
+	struct State result;
+	result.bits = 0;
+	uint32_t oldBits = oldState.bits;
 	for (int i = 31; i >= 0; --i) {
 		uint32_t neighborhood;
 		if (i == 0) {
-			neighborhood = ((bits << 1) & 0x06) | ((bits >> 31) & 0x01);
+			neighborhood = ((oldBits << 1) & 0x06) | ((oldBits >> 31) & 0x01);
 		} else if (i == 31) {
-			neighborhood = ((bits << 2) & 0x04) | ((bits >> 30) & 0x03);
+			neighborhood = ((oldBits << 2) & 0x04) | ((oldBits >> 30) & 0x03);
 		} else {
-			neighborhood = (bits >> (i - 1)) & 0x07;
+			neighborhood = (oldBits >> (i - 1)) & 0x07;
 		}
 
+		bool oldOn = getBit(oldBits, i);
 		bool newOn = (rule >> neighborhood) & 0x01;
-		result = (result << 1) | (newOn ? 0x01 : 0x00);
+		result.bits = (result.bits << 1) | (newOn ? 0x01 : 0x00);
+		result.birthDates[i] = !oldOn && newOn ? now : oldState.birthDates[i];
 	}
 
 	return result;
@@ -44,8 +47,14 @@ uint32_t calculateNewBits(uint32_t bits) {
 
 void setup() {
 	randomSeed(analogRead(0));
-	oldBits = random(0, 0xffff) << 16 | random(0, 0xffff);
-	newBits = calculateNewBits(oldBits);
+	
+	unsigned long now = millis();
+	
+	oldState.bits = random(0, 0xffff) << 16 | random(0, 0xffff);
+	for (int i = 0; i < 32; ++i) {
+		oldState.birthDates[i] = now;
+	}
+	newState = calculateNewState(oldState, now);
 
 	SPI.setBitOrder(MSBFIRST);
 	SPI.setDataMode(SPI_MODE0);
@@ -62,20 +71,36 @@ void loop() {
 	unsigned long now = millis();
 
 	while (now > (thisFlip + frameDuration)) {
-		oldBits = newBits;
-		newBits = calculateNewBits(newBits);
+		oldState = newState;
+		newState = calculateNewState(oldState, now);
 		thisFlip += frameDuration;
 	}
 
 	float progress = float(now - thisFlip) / frameDuration;
-	float blend = 3 * progress * progress - 2 * progress * progress * progress;
+	float brightnessBlend = 3 * progress * progress - 2 * progress * progress * progress;
 	byte buffer[32 * 3];
 	for (size_t i = 0, idx = 0; i < 32; ++i, idx += 3) {
-		byte oldBit = getBit(oldBits, i) ? 0xff : 0x00;
-		byte newBit = getBit(newBits, i) ? 0xff : 0x00;
-		buffer[idx] = (byte)(blend * newBit + (1 - blend) * oldBit);
-		buffer[idx + 1] = 0x00;
-		buffer[idx + 2] = 0x00;
+		float colorBlend = float(now - newState.birthDates[i]) / (3 * frameDuration);
+		colorBlend = colorBlend > 1 ? 1 : colorBlend;
+		rgb8 currentColor = newColor.interpolateTo(oldColor, colorBlend);
+		
+		byte oldBit = getBit(oldState.bits, i) ? 0xff : 0x00;
+		byte newBit = getBit(newState.bits, i) ? 0xff : 0x00;
+
+		rgb8 blendedColor;
+		if (!oldBit & !newBit) {
+			blendedColor = rgb8(0x00, 0x00, 0x00);
+		} else if (oldBit && newBit) {
+			blendedColor = currentColor;
+		} else if (!oldBit && newBit) {
+			blendedColor = rgb8(0, 0, 0).interpolateTo(currentColor, brightnessBlend);
+		} else if (oldBit && !newBit) {
+			blendedColor = currentColor.interpolateTo(rgb8(0, 0, 0), brightnessBlend);
+		}
+		
+		buffer[idx] = blendedColor.b;
+		buffer[idx + 1] = blendedColor.g;
+		buffer[idx + 2] = blendedColor.r;
 	}
 
 	digitalWrite(12, 0);
---
+++ CellularAutomata/RgbColor.h
@@ -0,0 +1,40 @@
+template <typename T>
+class RgbColor {
+  public:
+    RgbColor(): r(0), g(0), b(0) { }
+	RgbColor(T r, T g, T b): r(r), g(g), b(b) { }
+
+    T r;
+    T g;
+    T b;
+    
+    RgbColor<T> interpolateTo(const RgbColor<T> &target, float progress) {
+      RgbColor<T> result;
+      result.r = this->r + progress * (target.r - this->r);
+      result.g = this->g + progress * (target.g - this->g);
+      result.b = this->b + progress * (target.b - this->b);
+      return result;
+    }
+};
+
+template <typename T>
+RgbColor<T> operator - (const RgbColor<T> &lhs, const RgbColor<T> &rhs) {
+  return RgbColor<T>(lhs.r - rhs.r, lhs.g - rhs.g, lhs.b - rhs.b);
+}
+
+template <typename T>
+RgbColor<T> operator + (const RgbColor<T> &lhs, const RgbColor<T> &rhs) {
+  return RgbColor<T>(rhs.r + lhs.r, rhs.g + lhs.g, rhs.b + lhs.b);
+}
+
+template <typename T>
+RgbColor<T> operator * (const RgbColor<T> &lhs, float rhs) {
+  return RgbColor<T>(lhs.r * rhs, lhs.g * rhs, lhs.b * rhs);
+}
+
+template <typename T>
+RgbColor<T> operator * (float lhs, const RgbColor<T> &rhs) {
+  return rhs * lhs;
+}
+
+typedef RgbColor<uint8_t> rgb8;
